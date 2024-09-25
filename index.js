085f40dd5cd5965b2e3a10cbcdbeb833ac1f61d1
Panda max <remontf10@gmail.com>
2024-09-25 13:06:44 +0300

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-// import  tesseract  from "node-tesseract-ocr"
+import  tesseract  from "node-tesseract-ocr"
 // import fs from 'fs'
 import 'dotenv/config'
 import Ocr from '@gutenye/ocr-node'
 import { Telegraf } from 'telegraf'
 
 
-// const  config  =  { 
-//     lang : "eng" , 
-//     oem : 1 , 
-//     psm : 3 ,
+const  config  =  { 
+    lang : "rus" , 
+    oem : 1 , 
+    psm : 3 ,
     // load_system_dawg: 0,
     // tessedit_char_whitelist: "0123456789",
     // presets: ["tsv"]
-  // }
+}
 
-// const img = fs.readFileSync("./img/12.jpg")
 const bot = new Telegraf(process.env.BOT_TOKEN)
 const ocr = await Ocr.create()
 bot.on('message', async (ctx) => {
@@ -31,26 +30,21 @@ bot.on('message', async (ctx) => {
       await ctx.reply(`Товар № ${result.filter(item => item.text.split(' ').includes('796')).indexOf(i) + 1}` + `\nКоличество: ${data[data.length - 6]}` + `\nЦена: ${data[data.length - 5]}` + `\nСумма: ${data[data.length - 4]}` + `\nСумма с НДС: ${data[data.length - 1]}`)
     }
     await ctx.reply((Date.now() - time) / 1000 + ' ' + 'seconds')
+
+    // const img = fs.readFileSync(buffer)
+    tesseract.recognize(url.href, config) 
+    .then ((text)  =>  { 
+      console.log("Результат:\n", text) 
+    }) 
+    .catch((error)  =>  { 
+      console.log('Ошибка') 
+    })
   }
+
 })
 
-// const ocr = await Ocr.create()
-// const result = await ocr.detect(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/getFile?file_id=${ctx.message}`)
-// console.log(result.filter(item => item.text.includes('18%')))
-// console.log(result[10].text.split(' ')[result[10].text.split(' ').length - 1])
-// console.log(result[11])
-// console.log(result[12])
 
-// console.log(img)
 
-// tesseract 
-//   .recognize(img, config) 
-//   .then ((text)  =>  { 
-//     console . log ( "Результат:", text) 
-//   }) 
-//   .catch((error)  =>  { 
-//     console.log('Ошибкаэб', error.message) 
-//   })
 bot.launch()
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
